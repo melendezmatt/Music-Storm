effed732b83dab86c47eb2739174993e78a03c9a
Matt Melendez <matthewemelendez@gmail.com>
2021-06-21 15:37:37 -0700

    added track model/migration

---
+++ backend/db/migrations/20210621223545-create-track.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Tracks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model : 'Users'}
+      },
+      albumId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model : 'Tracks'}
+      },
+      url: {
+        allowNull: false,
+        type: Sequelize.STRING(255)
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING(75)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now')
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Tracks');
+  }
+};
---
+++ backend/db/models/track.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Track = sequelize.define('Track', {
+    userId: DataTypes.INTEGER,
+    albumId: DataTypes.INTEGER,
+    url: DataTypes.STRING,
+    title: DataTypes.STRING
+  }, {});
+  Track.associate = function(models) {
+    // associations can be defined here
+  };
+  return Track;
+};
