128b53823c38dedacd7a1a712a7b83ae1cd25666
Matt Melendez <matthewemelendez@gmail.com>
2021-06-22 14:59:17 -0700

    fixed bio, added albumTitle, and included mp3s

--- backend/db/migrations/20210622170255-Bio.js
+++ backend/db/migrations/20210622170255-Bio.js
@@ -2,13 +2,13 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('Users', 'Bio', {
+    return queryInterface.addColumn('Users', 'bio', {
       type: Sequelize.STRING(280),
       allowNull: false,
     })
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.removeColumn('Users', 'Bio')
+    return queryInterface.removeColumn('Users', 'bio')
   }
 };
